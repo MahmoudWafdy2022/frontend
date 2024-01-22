3c6283eeeddb48d5afa763a6ca194a7fbd3027cb
MahmoudWafdy2022 <108485206+MahmoudWafdy2022@users.noreply.github.com>
2024-01-22 18:55:57 +0200

    hello

--- src/slices/userApiSlice.js
+++ src/slices/userApiSlice.js
@@ -4,34 +4,34 @@ export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: "http://localhost:3001/users/login",
+        url: "https://backend-production-9647.up.railway.app/users/login",
         method: "POST",
         body: data,
       }),
     }),
     register: builder.mutation({
       query: (data) => ({
-        url: "http://localhost:3001/users/register",
+        url: "https://backend-production-9647.up.railway.app/users/register",
         method: "POST",
         body: data,
       }),
     }),
     logout: builder.mutation({
       query: () => ({
-        url: "http://localhost:3001/users/logout",
+        url: "https://backend-production-9647.up.railway.app/users/logout",
         method: "POST",
       }),
     }),
     profile: builder.mutation({
       query: (data) => ({
-        url: "http://localhost:3001/users/profile",
+        url: "https://backend-production-9647.up.railway.app/users/profile",
         method: "PUT",
         body: data,
       }),
     }),
     getUsers: builder.query({
       query: () => ({
-        url: "http://localhost:3001/users",
+        url: "https://backend-production-9647.up.railway.app/users",
       }),
       providesTags: ["User"],
       keepUnusedDataFor: 5,
@@ -39,7 +39,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
 
     getUserDetails: builder.query({
       query: (id) => ({
-        url: `http://localhost:3001/users/${id}`,
+        url: `https://backend-production-9647.up.railway.app/users/${id}`,
       }),
       keepUnusedDataFor: 5,
     }),
